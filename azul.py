8412bd9706a38e8515c2dc5d03b591bd046bb696
Josh Bellingham <108699936+jbcrash@users.noreply.github.com>
2023-01-07 18:12:53 -0700

    I made it so that the game asks how many players. Based on the number of players it lays out a certain number of "mats" as lists. I then added an input to "begin" which randomly takes tiles from the sac and places 4 on each mat. The next step is to figure out how to adapt the current system to fill the mats from the sac and THEN for the bucket, to keep the probability of seeing each tiles the same as in the board game. I have yet to add a gui, so right now all the tiles are moved in the terminal.

--- azul.py
+++ azul.py
@@ -5,6 +5,7 @@ Description: Coding an azul clone in Python partly for fun and
 partly so I can play the game on my TV and 
 partly so I can sell it to the game company and make a million dollars...
 '''
+import random
 
 #Make a class for tiles, and then make 100 tiles with 5 different colours
 
@@ -39,13 +40,149 @@ for x in range(1, 21):
     tilex = Tile("black", x)
     sac.append(tilex)
 
-print(sac)
-
 #Make a 'bucket' that store all the played game tiles
 
 #Make 'mats', which take the number of players and assign the appropriate number of playable spaces
+selecting = True
+while selecting == True:
+    players = int(input("How many players do you have?\n"))
 
-mat = [] #needs to have a limit of 4 tiles
+    try:
+        if players > 2:
+            print("Unfortunately, you need at least 2 players to play Azul. Go find some friends!")
+        if players == 2:
+            selecting = False
+            print("Setting out 5 mats...")
+            mat1 = []
+            mat2 = []
+            mat3 = []
+            mat4 = []
+            mat5 = []
+        if players == 3:
+            selecting = False
+            print("Setting out 7 mats...")
+            mat1 = []
+            mat2 = []
+            mat3 = []
+            mat4 = []
+            mat5 = []
+            mat6 = []
+            mat7 = []
+        if players == 4:
+            selecting = False
+            print("Setting out 9 mats...")
+            mat1 = []
+            mat2 = []
+            mat3 = []
+            mat4 = []
+            mat5 = []
+            mat6 = []
+            mat7 = []
+            mat8 = []
+            mat9 = []
+        if players > 4:
+            print("Sorry, Azul only supports up to 4 players at a time. You'll just have to take turns!")
+        else:
+            raise TypeError("Please enter an interger between 2 and 4.")
+    except TypeError:
+        print("")
+    
+begin = input("When you are ready to begin the game, type begin!")
+if begin == "begin":
+    if players == 2:
+        for i in range(4):
+            choice = random.choice(sac)
+            mat1.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat2.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat3.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat4.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat5.append(choice)
+            sac.remove(choice)
+        print("Mat 1: ", mat1, "\nMat 2: ", mat2, "\nMat 3: ", mat3, "\nMat 4: ", mat4, "\nMat 5: ", mat5)
+    if players == 3:
+        for i in range(4):
+            choice = random.choice(sac)
+            mat1.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat2.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat3.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat4.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat5.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat6.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat7.append(choice)
+            sac.remove(choice)
+        print("Mat 1: ", mat1, "\nMat 2: ", mat2, "\nMat 3: ", mat3, "\nMat 4: ", mat4, "\nMat 5: ", mat5,
+              "\nMat 6: ", mat6, "\nMat 7: ", mat7)
+    if players == 4:
+        for i in range(4):
+            choice = random.choice(sac)
+            mat1.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat2.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat3.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat4.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat5.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat6.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat7.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat8.append(choice)
+            sac.remove(choice)
+        for i in range(4):
+            choice = random.choice(sac)
+            mat9.append(choice)
+            sac.remove(choice)
+        print("Mat 1: ", mat1, "\nMat 2: ", mat2, "\nMat 3: ", mat3, "\nMat 4: ", mat4, "\nMat 5: ", mat5,
+              "\nMat 6: ", mat6, "\nMat 7: ", mat7, "\nMat 8: ", mat8, "\nMat 9: ", mat9)
+else:
+    print("Please type 'begin' to begin the game")
 
 #Make a function that fills the mats with 4 tiles each,, first from the sac, then from the bucket
 
